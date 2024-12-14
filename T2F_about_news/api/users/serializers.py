2ea88a6461d772c5186674652a2c5a281cb303b2
Александр Морозов <morozov460336@yandex.ru>
2024-12-14 06:47:03 +0300

    add decod base64

--- T2F_about_news/api/users/serializers.py
+++ T2F_about_news/api/users/serializers.py
@@ -1,6 +1,9 @@
-from djoser.serializers import UserCreateSerializer
+import base64
 
+from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
+
+from djoser.serializers import UserCreateSerializer
 
 from rest_framework import serializers
 
@@ -43,8 +46,23 @@ class CustomPasswordSerializer(serializers.Serializer):
         return value
 
 
+class Base64ImageField(serializers.ImageField):
+    """Сериализатор для декодирования изображений с фронтенда."""
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+
+
 class AvatarUserSerializer(serializers.ModelSerializer):
     """Сериализатор для работы с аватаром пользователя."""
+
+    avatar = Base64ImageField()
 
     class Meta:
         model = User
